784f3b0046c003c39cedaf6a96aa3584471e9972
Alexis <alexis.borja@gmail.com>
2022-09-18 22:16:06 -0700

    react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import Header from './Header/Header'
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./Header/Header";
+import Hats from "./Hats/Hats";
+import Accessories from "./Accessories/Accessories";
+import Clothing from "./Clothing/Clothing";
+import LandingPage from "./LandingPage/LandingPage";
+import Wigs from "./Wigs/Wigs";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="Marketplace">
       <header className="App-header">
         <Header />
-       
       </header>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<LandingPage />}></Route>
+
+          <Route path="/accessories" element={<Accessories />}></Route>
+
+          <Route exact path="/clothing" element={<Clothing />}></Route>
+          <Route exact path="/hats" element={<Hats />}></Route>
+
+          <Route exact path="/wigs" element={<Wigs />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/LandingPage/LandingPage.js
@@ -0,0 +1,9 @@
+function LandingPage() {
+  return (
+    <div>
+     Welcome to BOSS Marketplace
+    </div>
+  );
+}
+
+export default LandingPage;
