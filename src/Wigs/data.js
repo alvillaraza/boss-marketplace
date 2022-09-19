767946c73bfc3edf818caa05949f963aae736e5f
Alexis <alexis.borja@gmail.com>
2022-09-18 21:36:02 -0700

    wigs data done

---
+++ src/Wigs/data.js
@@ -0,0 +1,81 @@
+import black_curly_betty_boop from './assets/black_curly_betty_boop.png'
+import black_curly from './assets/black_curly.png'
+import blonde_betty_boop from './assets/blonde_betty_boop.jpeg'
+import blonde_curly from './assets/blonde_curly.jpeg'
+import blonde_wig from './assets/blonde_wig.jpeg'
+import cindy_lauper from './assets/cindy_lauper.jpeg'
+import grey_curly from './assets/grey_curly.jpeg'
+import long_red_wig from './assets/long_red_wig.jpeg'
+import neon_blue from './assets/neon_blue.jpeg'
+import orange_wig from './assets/orange_wig.jpeg'
+import red_queen from './assets/red_queen.jpeg'
+import red_wig from './assets/red_wig.jpeg'
+import short_white_wig from './assets/short_white_wig.jpeg'
+import white_wigs from './assets/white_wigs.jpeg'
+import yellow_blue_wig from './assets/yellow_blue_wig.jpeg'
+
+const wigsData = [
+    {
+        name: 'Black Curly Betty Boop Wig',
+        image: black_curly_betty_boop
+    },
+    {
+        name: 'Blonde Curly Betty Boop Wig',
+        image: blonde_betty_boop
+    },
+    {
+        name: 'Black Wavy Wig',
+        image: black_curly
+    },
+    {
+        name: 'Blonde Wavy Wig',
+        image: blonde_curly
+    },
+    {
+        name: 'Short Blonde Wig',
+        image: blonde_wig
+    },
+    {
+        name: 'Cindy Lauper Wig',
+        image: cindy_lauper
+    },
+    {
+        name: 'Grey Wavy Wig',
+        image: grey_curly
+    },
+    {
+        name: 'Long Red Wig',
+        image: long_red_wig
+    },
+    {
+        name: 'Neon Blue Wig',
+        image: neon_blue
+    },
+    {
+        name: 'Orange Lilu Wig',
+        image: orange_wig
+    },
+    {
+        name: 'Red Queen Wig',
+        image: red_queen
+    },
+    {
+        name: 'Short Red Wig',
+        image: red_wig
+    },
+    {
+        name: 'Short White Wig',
+        image: short_white_wig
+    },
+    {
+        name: 'White Wig',
+        image: white_wigs
+    },
+    {
+        name: 'Short Yellow Blue Wig',
+        image: yellow_blue_wig
+    },
+
+]
+
+export default wigsData
