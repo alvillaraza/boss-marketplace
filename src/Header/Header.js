1219c1873874337bcd50a7e4a7ef65b24a6c56d4
Alexis <alexis.borja@gmail.com>
2022-09-19 16:10:15 -0700

    WIP styling sections

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -2,24 +2,25 @@ import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <nav>
-      <Link to='/'>BOSS Marketplace</Link>
-      <Link to="/clothing">
-        <h2>Clothing</h2>
-      </Link>
-      |
-      <Link to="/accessories">
-        <h2>Accessories</h2>
-      </Link>
-      |
-      <Link to="/hats">
-        <h2>Hats</h2>
-      </Link>
-      |
-      <Link to="/wigs">
-        <h2>Wigs</h2>
-      </Link>
-    </nav>
+    <section className="header-wrapper">
+      <Link to="/">BOSS Marketplace</Link>
+      <nav>
+        <Link to="/clothing">
+          <h2>Clothing</h2>
+        </Link>
+        <Link to="/accessories">
+          <h2>Accessories</h2>
+        </Link>
+
+        <Link to="/hats">
+          <h2>Hats</h2>
+        </Link>
+
+        <Link to="/wigs">
+          <h2>Wigs</h2>
+        </Link>
+      </nav>
+    </section>
   );
 }
 
