9d88a1f741a6036b3e7599f015f009956d6651cb
Alexis <alexis.borja@gmail.com>
2022-09-18 21:38:24 -0700

    wigs displayed

--- src/Wigs/Wigs.js
+++ src/Wigs/Wigs.js
@@ -1,5 +1,19 @@
+import wigsData from "./data";
+
 function Wigs() {
-  return <div>Wigs</div>;
+  return (
+    <div>
+      wigs
+      {wigsData.map((wigs) => {
+        console.log(wigs);
+        return (
+          <>
+            <img src={wigs.image} alt={wigs.name} />
+          </>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Wigs;
