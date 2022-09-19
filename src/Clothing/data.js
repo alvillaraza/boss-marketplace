7e9b55d74e3f009a56d3a6a3900c5d7877d0f9a4
Alexis <alexis.borja@gmail.com>
2022-09-18 21:25:35 -0700

    Clothing Data Done

--- src/Clothing/data.js
+++ src/Clothing/data.js
@@ -43,53 +43,80 @@ import pirate_captain from "./assets/pirate_captain.jpeg";
 import pirates from "./assets/pirates.jpeg";
 import popsicles from "./assets/popsicles.jpeg";
 import race_car from "./assets/race_car.jpeg";
+import red_black_lace_leo from "./assets/red_black_lace_leo.jpeg";
+import red_feather_tutu from "./assets/red_feather_tutu.png";
+import red_mesh from "./assets/red_mesh.jpeg";
+import red_ninaj from "./assets/red_ninaj.jpeg";
+import retro_leopard from "./assets/retro_leopard.jpeg";
+import ring_leader from "./assets/ring_leader.jpeg";
+import rose_gold_leo from "./assets/rose_gold_leo.png";
+import salsa_latina from "./assets/salsa_latina.jpeg";
+import samba_girls_purple_blue from "./assets/samba_girls_purple_blue.jpeg";
+import samba_girls_purple_blue2 from "./assets/samba_girls_purple_blue2.jpeg";
+import samba_girls from "./assets/samba_girls.jpeg";
+import samba_girls2 from "./assets/samba_girls2.jpeg";
+import santa_hoodie from "./assets/santa_hoodie.jpeg";
+import shark from "./assets/shark.jpeg";
+import silver_black_raw_fashion from "./assets/silver_black_raw_fasion.jpeg";
+import silver_disco_leo from "./assets/silver_disco_leo.jpeg";
+import silver_fringe_belt from "./assets/silver_fringe_belt.jpeg";
+import sporty_football from "./assets/sporty_football.jpeg";
+import st_patricks from "./assets/st_patricks.jpeg";
+import tmnt from "./assets/tmnt.jpeg";
+import trapfest_cute from "./assets/trapfest_cute.jpeg";
+import white_crisscross from "./assets/white_crisscross.jpeg";
+import white_lace_fashion from "./assets/white_lace_fashion.jpeg";
+import white_mesh from "./assets/white_mesh.jpeg";
+import winter_white_leo from "./assets/winter_white_leo.jpeg";
+import xmas_winter_silver_skirt from "./assets/xmas_winter_silver_skirt.jpeg";
+import yellow_school_girl from "./assets/yellow_school_girl.jpeg";
 
 
 // data set should be name, image, number in Storage (count), items for sale, price?
 const clothingData = [
   {
     name: "apocalypse gold costume 2",
-    image:  apocalypse_gold_2 ,
+    image: apocalypse_gold_2,
   },
   {
     name: "apocalypse costume",
-    image: apocalypse_gold ,
+    image: apocalypse_gold,
   },
   {
     name: "audrey hepburn leotard",
-    image: audrey_hepburn ,
+    image: audrey_hepburn,
   },
   {
     name: "autumn babes outfit",
-    image: autumn_babes ,
+    image: autumn_babes,
   },
   {
     name: "autumn glitter",
-    image: autumn_full_piece ,
+    image: autumn_full_piece,
   },
   {
     name: "white lace full leotard",
-    image: white_lace_suit ,
+    image: white_lace_suit,
   },
   {
     name: "autumn babes outfit",
-    image: black_lacey_leo ,
+    image: black_lacey_leo,
   },
   {
     name: "bloody bunnies outfit",
-    image: bloody_bunnies ,
+    image: bloody_bunnies,
   },
   {
     name: "bloody bunnies outfit",
-    image: bloody_bunnies2 ,
+    image: bloody_bunnies2,
   },
   {
     name: "bloody bunnies outfit",
-    image: blue_sequin ,
+    image: blue_sequin,
   },
   {
     name: "bride of frankenstein",
-    image: bride_frankenstein ,
+    image: bride_frankenstein,
   },
   {
     name: "camo 2pc",
@@ -100,134 +127,241 @@ const clothingData = [
     image: christmas_tartan,
   },
   {
-      name: "comic book top",
-      image: comic_book,
-    },
+    name: "comic book top",
+    image: comic_book,
+  },
   {
-      name: "retro sailor outfit",
-      image: cute_sailor_retro,
-    },
+    name: "retro sailor outfit",
+    image: cute_sailor_retro,
+  },
   {
-      name: "futuristic silver",
-      image: dancetronauts,
-    },
+    name: "futuristic silver",
+    image: dancetronauts,
+  },
   {
-      name: "fembots",
-      image: fembots,
-    },
+    name: "fembots",
+    image: fembots,
+  },
   {
-      name: "futuristic silver 2pc",
-      image: futuristic_silver,
-    },
+    name: "futuristic silver 2pc",
+    image: futuristic_silver,
+  },
   {
-      name: "Oktoberfest",
-      image: german_oktoberfest_2,
-    },
+    name: "Oktoberfest",
+    image: german_oktoberfest_2,
+  },
   {
-      name: "Gold Disco Outfit",
-      image: gold_retro,
-    },
+    name: "Gold Disco Outfit",
+    image: gold_retro,
+  },
   {
-      name: "Green Mesh Leotard",
-      image: green_mesh,
-    },
+    name: "Green Mesh Leotard",
+    image: green_mesh,
+  },
   {
-      name: "Green Robe",
-      image: green_robe,
-    },
+    name: "Green Robe",
+    image: green_robe,
+  },
   {
-      name: "Green School Girl Golf Outfit",
-      image: green_school_girl_golf,
-    },
+    name: "Green School Girl Golf Outfit",
+    image: green_school_girl_golf,
+  },
   {
-      name: "Green St Patrick's Shamrock Leotard",
-      image: green_shamrock,
-    },
+    name: "Green St Patrick's Shamrock Leotard",
+    image: green_shamrock,
+  },
   {
-      name: "Halloween Candy",
-      image: halloween_candy,
-    },
+    name: "Halloween Candy",
+    image: halloween_candy,
+  },
   {
-      name: "Zombie Geisha",
-      image: halloween_geisha,
-    },
+    name: "Zombie Geisha",
+    image: halloween_geisha,
+  },
   {
-      name: "OG Harley Quinn",
-      image: harley_quinn_OG,
-    },
+    name: "OG Harley Quinn",
+    image: harley_quinn_OG,
+  },
   {
-      name: "Hooded Fur Leotard",
-      image: hooded_fur,
-    },
+    name: "Hooded Fur Leotard",
+    image: hooded_fur,
+  },
   {
-      name: "Strappy Hot Pink",
-      image: hot_pink_strappy,
-    },
+    name: "Strappy Hot Pink",
+    image: hot_pink_strappy,
+  },
   {
-      name: "Hula Girl",
-      image: hula_girl,
-    },
+    name: "Hula Girl",
+    image: hula_girl,
+  },
   {
-      name: "Incredibles Superhero",
-      image: incredibles,
-    },
+    name: "Incredibles Superhero",
+    image: incredibles,
+  },
   {
-      name: "Bond Girls",
-      image: james_bond,
-    },
+    name: "Bond Girls",
+    image: james_bond,
+  },
   {
-      name: "Ruffles",
-      image: latina_fluff_3,
-    },
+    name: "Ruffles",
+    image: latina_fluff_3,
+  },
   {
-      name: "Mariachi Inspired",
-      image: latina_mariachi,
-    },
+    name: "Mariachi Inspired",
+    image: latina_mariachi,
+  },
   {
-      name: "Lisa Frank Inspired",
-      image: lisa_frank,
-    },
+    name: "Lisa Frank Inspired",
+    image: lisa_frank,
+  },
   {
-      name: "Lisa Frank Inspired",
-      image: lisa_frank2,
-    },
+    name: "Lisa Frank Inspired",
+    image: lisa_frank2,
+  },
   {
-      name: "Mardi Gras",
-      image: mardi_gras,
-    },
+    name: "Mardi Gras",
+    image: mardi_gras,
+  },
   {
-      name: "Marie Antoinette",
-      image: marie_antoinette,
-    },
+    name: "Marie Antoinette",
+    image: marie_antoinette,
+  },
   {
-      name: "Mermaids",
-      image: mermaids,
-    },
+    name: "Mermaids",
+    image: mermaids,
+  },
   {
-      name: "Olive Gold Spikey 2 piece",
-      image: olive_gold_spikes,
-    },
+    name: "Olive Gold Spikey 2 piece",
+    image: olive_gold_spikes,
+  },
   {
-      name: "Pikachu",
-      image: pikachu,
-    },
+    name: "Pikachu",
+    image: pikachu,
+  },
   {
-      name: "Pirate Captain",
-      image: pirate_captain,
-    },
+    name: "Pirate Captain",
+    image: pirate_captain,
+  },
   {
-      name: "Pirates",
-      image: pirates,
-    },
+    name: "Pirates",
+    image: pirates,
+  },
   {
-      name: "The Bomb Pop Popsicles",
-      image: popsicles,
-    },
+    name: "The Bomb Pop Popsicles",
+    image: popsicles,
+  },
   {
-      name: "Racing Leotard",
-      image: race_car,
-    },
-
+    name: "Racing Leotard",
+    image: race_car,
+  },
+  {
+    name: "Black Red Lace Mesh Leotard",
+    image: red_black_lace_leo,
+  },
+  {
+    name: "Red Feather Tutu",
+    image: red_feather_tutu,
+  },
+  {
+    name: "Red Mesh Top",
+    image: red_mesh,
+  },
+  {
+    name: "Red Ninja",
+    image: red_ninaj,
+  },
+  {
+    name: "Retro Leopard Leotard",
+    image: retro_leopard,
+  },
+  {
+    name: "Ring Leader",
+    image: ring_leader,
+  },
+  {
+    name: "Rose Gold Leotard",
+    image: rose_gold_leo,
+  },
+  {
+    name: "Ruffley Rose Ensemble",
+    image: salsa_latina,
+  },
+  {
+    name: "Purple and Blue Samba Girls",
+    image: samba_girls_purple_blue,
+  },
+  {
+    name: "Purple and Blue Samba Girls",
+    image: samba_girls_purple_blue2,
+  },
+  {
+    name: "Samba Girls",
+    image: samba_girls,
+  },
+  {
+    name: "Green and Pink Samba Girls",
+    image: samba_girls2,
+  },
+  {
+    name: "Santa Hoodie Leotard",
+    image: santa_hoodie,
+  },
+  {
+    name: "Shark",
+    image: shark,
+  },
+  {
+    name: "Silver and Black Ensemble",
+    image: silver_black_raw_fashion,
+  },
+  {
+    name: "Silver Disco Leotard",
+    image: silver_disco_leo,
+  },
+  {
+    name: "Silver Fringe Belt",
+    image: silver_fringe_belt,
+  },
+  {
+    name: "Sporty Football Outfit",
+    image: sporty_football,
+  },
+  {
+    name: "St Patricks Outfit",
+    image: st_patricks,
+  },
+  {
+    name: "Teenage Mutant Ninja Turtle Leonardo",
+    image: tmnt,
+  },
+  {
+    name: "Trapfest 2 Pieces",
+    image: trapfest_cute,
+  },
+  {
+    name: "White Crisscross 2 Pieces",
+    image: white_crisscross,
+  },
+  {
+    name: "White lace Straps",
+    image: white_lace_fashion,
+  },
+  {
+    name: "White Mesh Leotard",
+    image: white_mesh,
+  },
+  {
+    name: "Winter White Leotard",
+    image: winter_white_leo,
+  },
+  {
+    name: "Silver Winter Skirt",
+    image: xmas_winter_silver_skirt,
+  },
+  {
+    name: "Yellow School Girl Outfit",
+    image: yellow_school_girl,
+  },
 ];
 
 export default clothingData;
