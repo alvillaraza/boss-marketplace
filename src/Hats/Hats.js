d596b0ac7b7046b712cf09bec071c67586840e06
Alexis <alexis.borja@gmail.com>
2022-09-19 20:24:46 -0700

    hats desc

--- src/Hats/Hats.js
+++ src/Hats/Hats.js
@@ -1,39 +1,15 @@
-import hatsData from './data'
-
-function Hats() {
-  return (
-    <div className='category-wrapper'>
-      Hats
-      {hatsData.map((hats) => {
-        console.log(hats);
-        return (
-          <>
-            <img src={hats.image} alt={hats.name} />
-          </>
-        );
-      })}
-    </div>
-  );
-}
-
-export default Hats;
-
 // import hatsData from './data'
-// import Description from '../Hats/Description';
 
 // function Hats() {
 //   return (
 //     <div className='category-wrapper'>
 //       Hats
 //       {hatsData.map((hats) => {
+//         console.log(hats);
 //         return (
-//           <div className="photo">
+//           <>
 //             <img src={hats.image} alt={hats.name} />
-//             <Description
-//               clothingName={hats.name}
-//               itemized={hats.available}
-//             />
-//           </div>
+//           </>
 //         );
 //       })}
 //     </div>
@@ -41,3 +17,27 @@ export default Hats;
 // }
 
 // export default Hats;
+
+import hatsData from './data'
+import Description from '../Hats/Description';
+
+function Hats() {
+  return (
+    <div className='category-wrapper'>
+      Hats
+      {hatsData.map((hats) => {
+        return (
+          <div className="photo">
+            <img src={hats.image} alt={hats.name} />
+            <Description
+              hatName={hats.name}
+              itemized={hats.available}
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Hats;
--- src/Hats/data.js
+++ src/Hats/data.js
@@ -20,71 +20,190 @@ const hatsData = [
   {
     name: "Black Leather Hat",
     image: leather_hat,
+    available: [
+      {
+        item: "black leather biker hat with silver chain",
+        price: 8,
+        count: 3,
+      },
+    ],
   },
   {
     name: "Red Military Hat",
     image: red_military,
+    available: [
+      {
+        item: "red military hat",
+        price: 12,
+        count: 2,
+      },
+    ],
   },
   {
-    name: "Freddy Kreuger Hat and Gloves",
+    name: "Freddy Kreuger Hat and Glove",
     image: freddy_with_hands,
+    available: [
+      {
+        item: "freddy kreuger hat and glove",
+        price: 12,
+        count: 2,
+      },
+    ],
   },
-  
-    {
-      name: "Leopard Fur Hood",
-      image: leopard_fur_front2,
-    },
-    {
-      name: "Mariachi Hat",
-      image: mariachi_front,
-    },
-    {
+
+  {
+    name: "Mariachi Hat",
+    image: mariachi_front,
+    available: [
+      {
+        item: "mariachi hat with moño",
+        price: 20,
+        count: 3,
+      },
+    ],
+  },
+  {
     name: "Patriotic Flowers Summer Hat",
     image: patriotic_summer_front,
+    available: [
+      {
+        item: "summer hat with flowers",
+        price: 8,
+        count: 3,
+      },
+    ],
   },
   {
     name: "Patriotic Feathers Tophat",
     image: patriotic_tophat_front,
+    available: [
+      {
+        item: "red, white, and blue tophat",
+        price: 10,
+        count: 3,
+      },
+    ],
   },
   {
     name: "Patriotic Visor",
     image: patriotic_visor_side,
+    available: [
+      {
+        item: "red, white, blue visor",
+        price: 8,
+        count: 3,
+      },
+    ],
   },
   {
     name: "Sailor Captain Hat",
     image: captain_hat_front,
+    available: [
+      {
+        item: "sailor captain hat",
+        price: 5,
+        count: 3,
+      },
+    ],
   },
   {
     name: "Sailor Hat",
     image: sailor_front,
+    available: [
+      {
+        item: "blue sailor hat",
+        price: 8,
+        count: 3,
+      },
+    ],
   },
   {
-    name: "Sailor Stripe Hat",
+    name: "Sailor Stripe Headband",
     image: sailor_stripe_front,
+    available: [
+      {
+        item: "sailor stripe headband",
+        price: 8,
+        count: 3,
+      },
+    ],
   },
   {
     name: "White Sailor",
     image: white_sailor_side,
+    available: [
+      {
+        item: "white sailor hat",
+        price: 5,
+        count: 3,
+      },
+    ],
   },
   {
     name: "Green Shamrock Bowler Hat",
     image: greenbowlerhat_side,
+    available: [
+      {
+        item: "green bowler hat with shamrock",
+        price: 15,
+        count: 3,
+      },
+    ],
   },
   {
     name: "Green Tophat",
     image: greentophat_side,
+    available: [
+      {
+        item: "green tophat",
+        price: 12,
+        count: 3,
+      },
+    ],
   },
   {
     name: "Green Tophat with Buckle",
     image: tophatwithbuckle,
+    available: [
+      {
+        item: "green tophat with buckle",
+        price: 15,
+        count: 2,
+      },
+    ],
   },
   {
     name: "White Fur Hood with Ears",
     image: white_fur_ears_front,
+    available: [
+      {
+        item: "white fur hood with ears",
+        price: 20,
+        count: 3,
+      },
+    ],
   },
   {
     name: "White Fur Hat",
     image: white_fur_hat,
+    available: [
+      {
+        item: "white fur  hat",
+        price: 15,
+        count: 2,
+      },
+    ],
+  },
+  {
+    name: "Leopard Fur Hood",
+    image: leopard_fur_front2,
+    available: [
+      {
+        item: "leopard fur hood",
+        price: 15,
+        count: 1,
+      },
+    ],
   },
 ];
 
