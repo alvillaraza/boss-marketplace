f6e2cbf49a24bcbebe1c46ad058b1d6961348f1d
Ray Villaraza <ray.villaraza@strongmind.com>
2022-10-07 13:05:07 -0700

    understanding decons

--- src/App.js
+++ src/App.js
@@ -1,85 +1,106 @@
-import React, { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./Header/Header";
-import Hats from "./Hats/Hats";
-import Accessories from "./Accessories/Accessories";
-import Clothing from "./Clothing/Clothing";
-import LandingPage from "./LandingPage/LandingPage";
-import Wigs from "./Wigs/Wigs";
-import MyList from "./MyList";
-import data from "./Clothing/data";
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './Header/Header';
+import Hats from './Hats/Hats';
+import Accessories from './Accessories/Accessories';
+import Clothing from './Clothing/Clothing';
+import LandingPage from './LandingPage/LandingPage';
+import Wigs from './Wigs/Wigs';
+import MyList from './MyList';
+import data from './Clothing/data';
 
-import "./App.css";
-import "./Normalize.css";
+import './App.css';
+import './Normalize.css';
 
 function App() {
-  const [list, setList] = useState(data);
-  console.log("this list", { list });
+    const [list, setList] = useState(data);
+    // console.log('this list', list);
 
-  const handleAdd = (selectedItem) => {
-    console.log("this iten", { selectedItem });
+    const handleAdd = (selectedItem) => {
+        console.log('...this item', { ...selectedItem });
+        /*
+            { item: 'audrey leotard' }
+        */
+        console.log('{ this item }', { selectedItem });
+        /* 
+            { selectedItem: 
+              { selectedItem:
+                { item: 'audrey leotard'}}}
+        */
+        console.log('this item', selectedItem);
+        /*
+            { item: 'audrey leotard' }
+        */
 
-    // item.count = item.count - 1;
-    // item.inCart = item.inCart + 1;
+        // setList((prev) => {
+        //   return prev.map((section) => {
+        //     return section.available.map((item) => {
+        //       if (selectedItem.item === item.item) {
+        //         console.log("item", item);
 
-    setList((prev) => {
-      return prev.map((section) => {
-        return section.available.map((item) => {
-          if (selectedItem.item === item.item) {
-            console.log("item", item);
+        //         return { ...item, count: item.count - 1, inCart: item.inCart + 1 };
+        //       }
 
-            return { ...item, count: item.count - 1, inCart: item.inCart + 1 };
-          }
+        //       return { ...item };
+        //     });
+        //   });
+        // });
+    };
 
-          return { ...item };
-        });
-      });
-    });
-  };
+    const handleRemove = (item) => {
+        item.count = item.count + 1;
 
-  const handleRemove = (item) => {
-    item.count = item.count + 1;
+        item.inCart = item.inCart - 1;
 
-    item.inCart = item.inCart - 1;
+        setList((current) =>
+            current.forEach((i) => {
+                return i.inCart - 1;
+            })
+        );
+    };
 
-    setList((current) =>
-      current.forEach((i) => {
-        return i.inCart - 1;
-      })
+    return (
+        <div className="Marketplace">
+            {/* <header className="App-header"> */}
+            <BrowserRouter>
+                <Header />
+                <Routes>
+                    <Route exact path="/" element={<LandingPage />}></Route>
+
+                    <Route
+                        path="/accessories"
+                        element={<Accessories handleAdd={handleAdd} />}
+                    ></Route>
+
+                    <Route
+                        path="/clothing"
+                        element={
+                            <Clothing
+                                handleAdd={handleAdd}
+                                handleRemove={handleRemove}
+                            />
+                        }
+                    ></Route>
+                    <Route
+                        path="/hats"
+                        element={<Hats handleAdd={handleAdd} />}
+                    ></Route>
+
+                    <Route
+                        path="/wigs"
+                        element={<Wigs handleAdd={handleAdd} />}
+                    ></Route>
+                    <Route
+                        path="/my-list"
+                        element={
+                            <MyList list={list} handleRemove={handleRemove} />
+                        }
+                    ></Route>
+                </Routes>
+            </BrowserRouter>
+            {/* </header> */}
+        </div>
     );
-  };
-
-  return (
-    <div className="Marketplace">
-      {/* <header className="App-header"> */}
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route exact path="/" element={<LandingPage />}></Route>
-
-          <Route
-            path="/accessories"
-            element={<Accessories handleAdd={handleAdd} />}
-          ></Route>
-
-          <Route
-            path="/clothing"
-            element={
-              <Clothing handleAdd={handleAdd} handleRemove={handleRemove} />
-            }
-          ></Route>
-          <Route path="/hats" element={<Hats handleAdd={handleAdd} />}></Route>
-
-          <Route path="/wigs" element={<Wigs handleAdd={handleAdd} />}></Route>
-          <Route
-            path="/my-list"
-            element={<MyList list={list} handleRemove={handleRemove} />}
-          ></Route>
-        </Routes>
-      </BrowserRouter>
-      {/* </header> */}
-    </div>
-  );
 }
 
 export default App;
--- src/Description.js
+++ src/Description.js
@@ -1,37 +1,41 @@
 function Description({ name, itemized, handleAdd, handleRemove }) {
-  return (
-    <div className="description-wrapper">
-      <h4 className="item-title">
-        <a
-          href={`mailto:someone@yoursite.com?subject=BOSS Marketplace ${name}`}
-        >
-          {name} <i className="fas fa-envelope"></i>
-        </a>
-      </h4>
-      {itemized.map((item, idx) => {
-        return (
-          <div className="description" key={idx}>
-            <div className="items">
-              <div
-                onClick={() => {
-                  handleAdd(item);
-                }}
-              >
-                <i className={`fas fa-plus  ${item.count <= 0 ? 'hide' : ''}`}></i>
-              </div>
+    return (
+        <div className="description-wrapper">
+            <h4 className="item-title">
+                <a
+                    href={`mailto:someone@yoursite.com?subject=BOSS Marketplace ${name}`}
+                >
+                    {name} <i className="fas fa-envelope"></i>
+                </a>
+            </h4>
+            {itemized.map((item, idx) => {
+                return (
+                    <div className="description" key={idx}>
+                        <div className="items">
+                            <div
+                                onClick={() => {
+                                    handleAdd(item);
+                                }}
+                            >
+                                <i
+                                    className={`fas fa-plus  ${
+                                        item.count <= 0 ? 'hide' : ''
+                                    }`}
+                                ></i>
+                            </div>
 
-              <span> {item.item}</span>
-            </div>
-            <p>
-              Price: <span>${item.price}</span>
-            </p>
-            <p>
-              # available: <span>{item.count}</span>
-            </p>
-          </div>
-        );
-      })}
-    </div>
-  );
+                            <span> {item.item}</span>
+                        </div>
+                        <p>
+                            Price: <span>${item.price}</span>
+                        </p>
+                        <p>
+                            # available: <span>{item.count}</span>
+                        </p>
+                    </div>
+                );
+            })}
+        </div>
+    );
 }
 export default Description;
