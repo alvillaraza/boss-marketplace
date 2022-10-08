058e7664e270b9002a21ea0e8bf01176c45df0a6
Alexis <alexis.borja@gmail.com>
2022-10-08 14:20:47 -0700

    got dropdown to werk!!!

--- src/EntireStore.js
+++ src/EntireStore.js
@@ -1,41 +1,55 @@
-import React, { useState } from 'react';
-import allData from './allData';
+import React, { useState } from "react";
+import allData from "./allData";
+import Description from "./Description";
 
 const EntireStore = () => {
-    const [category, setCategory] = useState('all')
-    console.log('category', category)
-    console.log(allData)
-    function handleChange(e) {
-         setCategory(e.target.value)
-      console.log('e', e.target.value)
-      }
+  const [category, setCategory] = useState("all");
+  console.log("category", category);
+  console.log(allData);
+  function handleChange(e) {
+    setCategory(e.target.value);
+    console.log("e", e.target.value);
+  }
 
-      function handleSubmit(e) {
-        e.preventDefault();
-      }
-    return (
-        <section>Entire Store
-             <form onSubmit={handleSubmit}>
-          <select
-            className="dropdown"
-            onChange={handleChange}
-            // value={props.currentCategory}
-            aria-label="dropdown"
-          >
-            <option value="" placeholder="see all products">
-              See all Products
-            </option>
-            <option value="clothing">Clothing</option>
-            <option value="hats">Hats</option>
-            <option value="wigs">Wigs</option>
-            <option value="accessories">Accessories</option>
-          </select>
-        </form>
-            <div>
-                
-        </div>
-        </section>
-    )
-}
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+  return (
+    <section>
+      Entire Store
+      <form onSubmit={handleSubmit}>
+        <select
+          className="dropdown"
+          onChange={handleChange}
+          // value={props.currentCategory}
+          aria-label="dropdown"
+        >
+          <option value="" placeholder="see all products">
+            See all Products
+          </option>
+          <option value="clothing">Clothing</option>
+          <option value="hats">Hats</option>
+          <option value="wigs">Wigs</option>
+          <option value="accessories">Accessories</option>
+        </select>
+      </form>
+      <div className="category-wrapper">
+        {allData.map((item, idx) => {
+          if (item.category === category) {
+            return (
+              <div className="photo" key={idx}>
+                <img src={item.image} alt={item.name} />
+                <Description
+                  name={item.name}
+                  itemized={item.available}
+                />
+              </div>
+            );
+          }
+        })}
+      </div>
+    </section>
+  );
+};
 
 export default EntireStore;
